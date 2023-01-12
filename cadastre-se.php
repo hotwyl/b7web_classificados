9f0262e3c412938220417af080bea41c4fc50882
William Camargo <44587916+hotwyl@users.noreply.github.com>
2023-01-11 21:28:25 -0300

    classe usuario com funcionalidade + telas de cadastro, login e logout

--- cadastre-se.php
+++ cadastre-se.php
@@ -13,7 +13,21 @@
                 $telefone = addslashes($_POST['telefone']);
 
                 if(!empty($nome)&& !empty($email) && !empty($senha)){
-                    $u->cadastrar($nome, $email, $senha, $telefone);
+                    if($u->cadastrar($nome, $email, $senha, $telefone)){
+                        ?>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>Parabéns!</strong> Cadastrado com sucesso. <a href="./login.php" class="alert-link">Faça o login agora.</a>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Este usuário já existe!</strong> <a href="./login.php" class="alert-link">Faça o login agora.</a>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php
+                    }
                 } else{
                     ?>
                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
--- config.php
+++ config.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+global $pdo;
+
 try {
     $pdo = new PDO("mysql:dbname=hg3won41_classificados;host=nspro32.hostgator.com.br", "hg3won41_hotwyl", "willfrombrasil");
 } catch (PDOException $e) {
